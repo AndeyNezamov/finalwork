2fbdf64ae2f2eac9c626665c2d94954a30c06dfe
Nezamov.Andrei <lynch.0945@gmail.com>
2023-03-17 22:24:16 +0900

    c#

---
+++ homework/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] StringArray(string[] array)
+{
+    string[] MyArray = new string[array.Length];
+    int ElementsCount = 0; 
+    for(int i=0; i < array.Length; i++)
+        if(array[i].Length <= 3)
+    {
+            MyArray[ElementsCount++] = array[i];
+    }
+    string[] result = new string[ElementsCount];
+    for(int i = 0; i < ElementsCount; i++)
+        result[i] = MyArray[i];
+    return result;
+}
+void InputArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{i+1} элемент: ");
+        array[i] = Console.ReadLine();
+    }
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.WriteLine(array[i]);
+}
+
+Console.Write("Введите колличество элементов: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] myStringArray = new string[size];
+InputArray(myStringArray);
+string[] processedArray = StringArray(myStringArray);
+Console.WriteLine("В массиве элементов <= 3: ");
+ShowArray(processedArray);
